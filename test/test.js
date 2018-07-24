e150dd8a0aed0ae153fe92fb366da97bffb3a45d
Justin J. Novack <jnovack@gmail.com>
2018-07-24 14:41:42 -0400

    add tests for replay parsing

--- test/test.js
+++ test/test.js
@@ -4,10 +4,31 @@ const fs = require("fs");
 const expect = require('chai').expect;
 const parser = require('../parser.js');
 
+const HeroesTalents = require('heroes-talents');
+const Heroes = new HeroesTalents.HeroesTalents('node_modules/heroes-talents/' + 'assets/heroes-talents', 'node_modules/heroes-talents/' + 'assets/data');
+
+
+const readJson = (path, cb) => {
+  fs.readFile(require.resolve(path), (err, data) => {
+    if (err)
+      cb(err)
+    else
+      cb(null, JSON.parse(data))
+  })
+}
+const towers = parser.processReplay('test/replays/towers-of-doom.StormReplay', Heroes);
+
 describe('parser.js', function () {
     it('parser version 6', function () {
-        var result = parser.VERSION;
+        let result = parser.VERSION;
         expect(result).to.be.equal(6);
+    });
+
+    it('json deep equal', function () {
+		readJson(__dirname + '/towers-of-doom.json', (err, settings) => {
+			expect(towers).to.be.deep.equal(settings)
+			done(err)
+	    });
     });
 
 });
