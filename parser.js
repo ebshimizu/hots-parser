8d83149dd8ebd10b6583c647fa6715b959345b39
Evan Shimizu <ebshimizu@gmail.com>
2018-11-13 19:05:28 -0500

    2.40

--- parser.js
+++ parser.js
@@ -7,8 +7,8 @@ const PARSER_VERSION = 6;
 const XRegExp = require('xregexp');
 const attrs = require('./attr.js');
 
-// 2.39.2.69823
-const MAX_SUPPORTED_BUILD = 69823;
+// 2.40.0.70200
+const MAX_SUPPORTED_BUILD = 70200;
 
 const BSTEP_FRAME_THRESHOLD = 6;
 
