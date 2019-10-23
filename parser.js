05937ef4881916cf7e4d24494b2f6ba9aa4ee0a1
Evan Shimizu <ebshimizu@gmail.com>
2019-10-23 17:57:12 -0400

    hotfix for 2.48.2.76893

--- parser.js
+++ parser.js
@@ -10,8 +10,8 @@ const attrs = require('./attr.js');
 // uncomment for debug
 // log.level = 'trace';
 
-// 2.48.2.76781
-const MAX_SUPPORTED_BUILD = 76781;
+// 2.48.2.76893
+const MAX_SUPPORTED_BUILD = 76893;
 
 const BSTEP_FRAME_THRESHOLD = 8;
 
