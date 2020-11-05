26acb7b365e108ca8bc213d7fe4bbce7190433bd
Evan Shimizu <ebshimizu@gmail.com>
2020-11-05 00:20:24 -0500

    patch 2.52.2

--- parser.js
+++ parser.js
@@ -10,8 +10,8 @@ const attrs = require('./attr.js');
 // uncomment for debug
 // log.level = 'trace';
 
-// 2.52.1.82169
-const MAX_SUPPORTED_BUILD = 82169;
+// 2.52.2.82624
+const MAX_SUPPORTED_BUILD = 82624;
 
 const BSTEP_FRAME_THRESHOLD = 8;
 
