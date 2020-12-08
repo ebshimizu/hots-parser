3d001d1107678c53f7b97ee9276f72396b6300eb
Evan Shimizu <ebshimizu@gmail.com>
2020-12-07 19:27:28 -0500

    hot feex

--- parser.js
+++ parser.js
@@ -10,8 +10,8 @@ const attrs = require('./attr.js');
 // uncomment for debug
 // log.level = 'trace';
 
-// 2.53.0.83077
-const MAX_SUPPORTED_BUILD = 83077;
+// 2.53.0.83086
+const MAX_SUPPORTED_BUILD = 83086;
 
 const BSTEP_FRAME_THRESHOLD = 8;
 
