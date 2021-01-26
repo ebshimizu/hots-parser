b7ff8cec97d22428dd03760f918176de1a76170c
Evan Shimizu <ebshimizu@gmail.com>
2021-01-25 19:32:46 -0500

    hotfix 83716

--- parser.js
+++ parser.js
@@ -10,8 +10,8 @@ const attrs = require('./attr.js');
 // uncomment for debug
 // log.level = 'trace';
 
-// 2.53.1.83632
-const MAX_SUPPORTED_BUILD = 83632;
+// 2.53.1.83716
+const MAX_SUPPORTED_BUILD = 83716;
 
 const BSTEP_FRAME_THRESHOLD = 8;
 
