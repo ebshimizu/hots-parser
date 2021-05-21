1e088b020f66fe35e646718fb6bdc21f1a9332b3
Evan Shimizu <ebshimizu@gmail.com>
2021-05-20 20:30:34 -0400

    7.54

--- parser.js
+++ parser.js
@@ -10,8 +10,8 @@ const attrs = require('./attr.js');
 // uncomment for debug
 // log.level = 'trace';
 
-// 2.53.3.84200
-const MAX_SUPPORTED_BUILD = 84249;
+// 2.54.0.85027
+const MAX_SUPPORTED_BUILD = 85027;
 
 const BSTEP_FRAME_THRESHOLD = 8;
 
